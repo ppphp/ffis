3f477507e1eee652d833050e8451ef3f29b35a1c
ppphp <kevinniub@gmail.com>
2019-10-01 03:21:19 +0800

    update js

---
+++ script/javascript/README.md
@@ -0,0 +1,3 @@
+# node-ffi
+
+only work on node 10-, fail to install ffi module on node 12
--- script/javascript/index.js
+++ script/javascript/index.js
@@ -1,11 +1,10 @@
 var ffi = require("ffi");
-var ref = require('ref');
-
+var ref = require("ref");
 
 var hello = ffi.Library("../../lib/libmylib.so", {
-    "add": [int, [int, int]],
-//    "minus": [int, [int, int]],
-//    "multiply": [int, [int, int]]
+  add: ["int", ["int", "int"]]
+  //    "minus": [int, [int, int]],
+  //    "multiply": [int, [int, int]]
 });
 
 process.stdout.write("hello: ");
